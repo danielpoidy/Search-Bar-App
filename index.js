688bbc5ee26449982332c48a2a518f4052d8abe6
danielpoidy <poidydaniel@gmail.com>
2020-08-12 13:54:39 +0000

    Adding regex to prevent spaces

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 
 document.getElementById('inputsearch').addEventListener('keyup', function (event) {
   let searchName = document.getElementById('inputsearch')
-  searchName = searchName.value.toLowerCase()
+  searchName = searchName.value.toLowerCase().replace(/\s+/g, '')
+  console.log(searchName)
 
   let allNames = document.getElementsByClassName('name')
 
   for (let i = 0; i < allNames.length; i++) {
-    const currentName = allNames[i].textContent.toLocaleLowerCase()
+    const currentName = allNames[i].textContent.toLocaleLowerCase().replace(/\s+/g, '')
     if (currentName.includes(searchName)) {
       allNames[i].style.display = 'block'
     } else {
